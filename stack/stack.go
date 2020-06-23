5fa240a4d948f9f3695ba6c804bec9ff9e29ceef
hyx360 <34064523+hyx360@users.noreply.github.com>
2020-06-23 21:43:15 +0800

    Update stack.go

--- stack/stack.go
+++ stack/stack.go
@@ -3,23 +3,23 @@ package stack
 type Item interface {
 }
 
-// ItemStack the stack of items
+// ItemStack：保存栈的item
 type ItemStack struct {
     items []Item
 }
 
-// New Create a new ItemStack
+// 新建一个ItemStack
 func (s *ItemStack) New() *ItemStack {
     s.items = []Item{}
     return s
 }
 
-// Push adds an Item to the top of the stack
+// 添加item到栈顶端
 func (s *ItemStack) Push(t Item) {
     s.items = append(s.items, t)
 }
 
-// Pop removes an Item from the top of the stack
+// 从栈顶端移除一个item
 func (s *ItemStack) Pop() *Item {
     item := s.items[len(s.items)-1] // 后进先出
     s.items = s.items[0:len(s.items)-1]
