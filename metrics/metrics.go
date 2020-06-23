a0490d806bff13bdbcd76b4416a763db14f0a381
root <2351848492@qq.com>
2020-06-23 21:40:18 +0800

    added files

---
+++ metrics/metrics.go
@@ -0,0 +1,68 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"time"
+	"strconv"
+)
+
+var (
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:      "request_total",
+			Help:      "Number of request processed by this service.",
+		}, []string{},
+	)
+
+	requestLatency = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:      "request_latency_seconds",
+			Help:      "Time spent in this service.",
+			Buckets:   []float64{0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0, 60.0, 120.0, 300.0},
+		}, []string{},
+	)
+	requestTimeStamp = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name:	   "request_timestamp",
+			Help:      "Timestamp of request processed by this service.",
+	})
+)
+
+// AdmissionLatency measures latency / execution time of Admission Control execution
+// usual usage pattern is: timer := NewAdmissionLatency() ; compute ; timer.Observe()
+type RequestLatency struct {
+	histo *prometheus.HistogramVec
+	start time.Time
+}
+
+func Register() {
+	prometheus.MustRegister(requestCount)
+	prometheus.MustRegister(requestLatency)
+	prometheus.MustRegister(requestTimeStamp)
+}
+
+
+// NewAdmissionLatency provides a timer for admission latency; call Observe() on it to measure
+func NewAdmissionLatency() *RequestLatency {
+	return &RequestLatency{
+		histo: requestLatency,
+		start: time.Now(),
+	}
+}
+
+// Observe measures the execution time from when the AdmissionLatency was created
+func (t *RequestLatency) Observe() {
+	(*t.histo).WithLabelValues().Observe(time.Now().Sub(t.start).Seconds())
+}
+
+
+// RequestIncrease increases the counter of request handled by this service
+func RequestIncrease() {
+	requestCount.WithLabelValues().Add(1)
+	
+	t_string := strconv.FormatInt(time.Now().Unix(),10)
+	t_float, _ := strconv.ParseFloat(t_string,64)
+	requestTimeStamp.Set(t_float)
+}
+
+
